924719975ea5a4790da49ee8172364d3203f74a5
Jovana Kovacevic <jovana1005@gmail.com>
2022-06-03 18:26:54 +0200

    add comments to js code

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function getDaysInMonth(year, month) {
   return new Date(year, month + 1, 0).getDate()
 }
 
+// function that adds a button element
 function appendTitleWrapperButton(innerText) {
   const button = document.createElement('button')
   button.innerText = innerText
@@ -40,36 +41,40 @@ function appendTitleWrapperButton(innerText) {
   return button
 }
 
+// create new Date object
 const date = new Date()
+// get current month
 const currentMonth = date.getMonth()
+// get current year
 const currentYear = date.getFullYear()
+// get current date
 const dayOfMonth = date.getDate()
 
-// Div containing
-// - title (name of current year)
-// - title (name of current month)
-// - buttons for the previous and next month
 const body = document.body
 
+// Div containing
+// - title (name of current year and month)
+// - buttons for the previous and next month
 const titleWrapper = document.createElement('div')
 titleWrapper.id = "title-wrapper"
 body.append(titleWrapper)
 
+// add "previous" button
 const previous = appendTitleWrapperButton("<")
 
-let nameOfCurrentMonth = MONTH_NAMES[currentMonth]
-
+// create h1 element for the title
 let title = document.createElement('h1')
 titleWrapper.append(title)
 
+// add "next" button
 const next = appendTitleWrapperButton(">")
 
-// Div containing days of week
-
+// div containing days of week
 const weekDays = document.createElement('div')
 weekDays.id = "week-days"
 body.append(weekDays)
 
+// populate week days
 for (let i = 0; i < DAY_NAMES.length; i++) {
   let weekDayCell = document.createElement('div')
   weekDayCell.className = "cell"
@@ -77,40 +82,51 @@ for (let i = 0; i < DAY_NAMES.length; i++) {
   weekDays.append(weekDayCell)
 }
 
-// Div containing table cells with the days in the current month
-
+// div containing table cells with days of current month
 const calendarWrapper = document.createElement('div')
 calendarWrapper.id = "wrapper"
 body.append(calendarWrapper)
 
+// function that clears the html before rendering new page
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild)
   }
 }
 
-// creates table cells and populates them with numbers representing each day in the month
-
+// create table cells and populates them with numbers representing each day in the month
 function renderMonth(month, year) {
   removeAllChildNodes(calendarWrapper)
   let counter = 1
   const daysInMonth = getDaysInMonth(year, month)
+
+  // check what week day is the first day in current month
   const checkDay = new Date(year, month)
   let firstWeekDayInMonth = checkDay.getDay()
+
+  // assign index to the first day in current month 
+  // so that the week starts from Monday
   firstWeekDayInMonth = firstWeekDayInMonth === 0 ? 7 : firstWeekDayInMonth
 
+  // populate the month days cells
   for (let i = 0; i < daysInMonth; i++) {
     let cell = document.createElement('div')
     cell.className = "cell"
     calendarWrapper.append(cell)
     cell.innerText = counter
 
+    // add an id to the cell representing the first day in month
     if (cell.innerText === "1") {
       cell.id = "date1"
     }
 
+    // change style of the grid so that the cell representing 
+    // the first day in current month is located at the appropriate
+    // cell representing the first week day of current month
     document.getElementById("date1").style.gridColumnStart = firstWeekDayInMonth
 
+    // add an id to the cell representing current day
+    // in order to highlight it in css
     if (counter === dayOfMonth
       && month === date.getMonth()
       && year === date.getFullYear()) {
@@ -118,6 +134,7 @@ function renderMonth(month, year) {
     }
     counter++
   }
+  // populate title with current month and year
   title.innerText = `${MONTH_NAMES[month]} ${year}`
 }
 
@@ -125,8 +142,7 @@ let renderedMonth = currentMonth
 let renderedYear = currentYear
 renderMonth(renderedMonth, renderedYear)
 
-// creates click event for previous button
-
+// create click event for previous button
 function getPreviousMonth() {
   renderedMonth--
   if (renderedMonth === -1) {
@@ -138,8 +154,7 @@ function getPreviousMonth() {
 
 previous.addEventListener('click', getPreviousMonth)
 
-// creates click event for next button
-
+// create click event for next button
 function getNextMonth() {
   renderedMonth++
   if (renderedMonth === 12) {
